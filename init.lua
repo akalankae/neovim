4b8780d2af6513e3911dfb7f94a79c1039948fbd
akalanka <akalankae@gmail.com>
2022-02-01 12:47:03 +0530

    Removed redundant comments

--- init.lua
+++ init.lua
@@ -2,15 +2,17 @@
 -- init.lua
 -- Lua based neovim configuration
 --------------------------------------------------------------------------------------
--- Setup autocommands written in vimscript
+-- NOTES:
+-- For you to jump to files and directories in require statements with "gf", you need
+-- to put a lua config file in $HOME/.config/nvim/after/ftplugin directory
+-- (e.g paths.lua) so that it will be loaded automatically.
+--------------------------------------------------------------------------------------
+
+-- Source vimscript source files
 vim.cmd("source " .. vim.fn.stdpath("config") .. "/vimscript/autocommands.vim")
 
 -- Configs for the user
 require("user")
--- require("user/keymaps")
--- require("user/options")
--- require("user/plugins")
 
 -- Configs for installed plugins
 require("plugins")
--- require("plugins/lualine")
