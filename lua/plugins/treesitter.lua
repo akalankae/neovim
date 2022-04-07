cf21e0c18aa166e8999d64644b11a4fad4b99aa5
akalanka <akalankae@gmail.com>
2022-04-07 20:36:06 +0530

    Added configs for rainbow parenthesis plugin & disabled treesitter based indentation for C

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,11 +13,12 @@ require("nvim-treesitter.configs").setup {
     -- `false` will disable the whole extension
     enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
+    -- NOTE: these are the names of the parsers and not the filetype.
+    -- (e.g. if you want to disable highlighting for the `tex` filetype, you
+    -- need to include `latex` in this list as this is the name of the parser)
     -- list of language that will be disabled
-    disable = { "c", "rust" },
+    -- treesitter parser for Rust is twice as slow (apparently) as native rustc
+    disable = { "rust" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -29,5 +30,17 @@ require("nvim-treesitter.configs").setup {
   -- EXPERIMENTAL indentation based on treesitter for "=" operator
   indent = {
     enable = true,
+    -- List of languages to diable treesitter based indentation
+    disable = { "c" },
+  },
+
+  -- Configs for treesitter based rainbow parenthesis plugin
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
   }
 }
