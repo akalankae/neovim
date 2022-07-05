f4105f9ba24d12fd9767e2aa4b167f6583b4715e
akalanka <akalankae@gmail.com>
2022-07-06 03:41:38 +0530

    Added diagnostics table to the nvim-tree setup function

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,13 +1,23 @@
 -- Configs for nvim-tree plugin for file system navigation
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = { key = "u", action = "dir_up" },
-    },
-  },
-  renderer = { group_empty = true },
-  filters = { dotfiles = true }
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = { key = "u", action = "dir_up" },
+		},
+	},
+	renderer = { group_empty = true },
+	filters = { dotfiles = true },
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
 })
