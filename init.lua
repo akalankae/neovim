bf5079a4b21a533d1884e1a7578911c98b3b24e5
akalanka <akalankae@gmail.com>
2022-07-15 20:20:17 +0530

    Added lua statusline, lualine, and it's config file

--- init.lua
+++ init.lua
@@ -4,3 +4,5 @@ require("user.settings")
 require("user.autocommands")
 require("user.keymappings")
 require("user.plugins") --> "user/plugins.lua" requires "plugins/packer.lua"
+
+require("plugins.lualine")
